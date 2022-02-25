31d60828c85f1c7fbaefe31131ea4bbefebdfe46
jimmy-liu <jimmy505024@gmail.com>
2022-02-25 16:17:35 +0800

    fix

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+yes | sudo pip install --upgrade opencv-python
+yes | sudo pip install openslide-python
+yes | sudo apt-get update
+yes | sudo apt-get install openslide-tools
+yes | sudo apt install -y libgl1-mesa-glx
+yes | sudo apt install libsm6
+yes | sudo apt install libgl1-mesa-glx
+yes | sudo apt-get install libxrender1
+yes | sudo apt-get install libvips
+yes | pip install tqdm
+yes | pip install pandas
+yes | pip install scipy
